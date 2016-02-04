c32e0857e8c892cfe6e267e5b79cae0b8fd4070e
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-04 12:16:23 -0600

    added student file

---
+++ Oscars.rb
@@ -0,0 +1,27 @@
+#It's Oscar night! Being a huge movie buff, you've predicted the winner of the categories "Best Picture", "Best Actress", "Best Actor" Create a hash where your predictions are
+#assigned to the correct categories
+
+
+#Whoops, you forgot a category. Add a prediction to the hash for the category "Best Animated Film"
+
+
+#You've talked to your friends and they've convinced you you're wrong about your Best Actor prediction. Change your prediction to somebody else
+
+
+#Let's see what you've predicted! Iterate through your hash and print a sentence for each category/prediction (For example. "The winner of Best Picture will definitely be Room
+#and you're crazy if you think otherwise!")
+
+
+#Play around with 3 other hash methods to answer some questions about your predictions. For example, did you predict "The Revenant" would win anything? How many awards did you predict? etc.
+
+
+#Bonus: The Oscar winners list leaked! Let's see how many we got correct. Iterate through your prediction hash and the winners hash and print out a sentence for each awards
+#you guessed correctly (Example: I knew Inside Out would win Best Animated Film!). Hint: make sure the names of the keys are the same!
+
+oscar_winners = {"Best Picture" =>"Spotlight", "Best Actress" => "Brie Larson", "Best Actor" => "Leonardo DiCaprio", "Best Animated Film" => "Inside Out"}
+
+
+#Second Bonus: Oh no, you made a typo in your prediction hash! You said "Spitlight" was going to win best picture instead of "Spotlight". Change your prediction hash to reflect this. How might you
+#make your algorithm for detecting correct guesses slightly less strict? Talk about different tactics before trying to implement any.
+
+
